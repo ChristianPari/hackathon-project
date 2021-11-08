54343073a83775ee805d9ca39c51facfbacaa64b
Freddy Canido <freddy.canido12@gmail.com>
2021-11-07 19:18:00 -0500

    added delete mapping and ticketnotfound error

---
+++ ticket-service/src/main/java/com/hackathon/ticketservice/TicketNotFound.java
@@ -0,0 +1,15 @@
+package com.hackathon.ticketservice;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class TicketNotFound extends RuntimeException{
+    public TicketNotFound() {
+        super("Ticket not found");
+    }
+
+    public TicketNotFound(String message) {
+        super(message);
+    }
+}
