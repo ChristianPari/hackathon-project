b7284d4e67b7077ea488c952a0dd938541505f88
Christian Pari <christianpari@outlook.com>
2021-11-19 19:50:42 -0500

    ugh

--- user-service/src/main/java/com/hackathon/userservice/models/User.java
+++ user-service/src/main/java/com/hackathon/userservice/models/User.java
@@ -24,10 +24,6 @@ public class User {
     private String lastName;
     private String email;
     private String password;
-<<<<<<< HEAD
-    private Integer[] departments; // 0 - Gen, 1 - Back, 2 - Front
-=======
     private String departments; // general | back | front | both
->>>>>>> 535a2586e7454940c97264336a8963c23e1aad53
     private String[] roles;
 }
